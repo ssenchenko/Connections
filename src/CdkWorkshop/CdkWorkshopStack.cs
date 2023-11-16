08dacba203a4d360d92a3bd8f276ac4916eecd41
Slava Senchenko <13832305+ssenchenko@users.noreply.github.com>
2023-11-15 16:17:02 -0800

    Initial commit

---
+++ src/CdkWorkshop/CdkWorkshopStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace CdkWorkshop
+{
+    public class CdkWorkshopStack : Stack
+    {
+        internal CdkWorkshopStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
